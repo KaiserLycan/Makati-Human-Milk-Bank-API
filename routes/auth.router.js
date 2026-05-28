956c83b120e5b771db9d68fd81aaab11ae0cf2eb
Joseph Rey R. Panti <josephreyramirezpanti+github@gmail.com>
2026-05-29 07:03:58 +0800

    feat: Session tokens

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import {Authenticate} from "../controllers/auth.controller.js";
+import {Authenticate, RefreshAccessToken} from "../controllers/auth.controller.js";
+import {ProtectRoute} from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/login", Authenticate)
+router.post("/refresh-token", ProtectRoute, RefreshAccessToken)
 
 export default router;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import app from "./app.js";
+import {redis} from "./lib/redis.lib.js";
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
