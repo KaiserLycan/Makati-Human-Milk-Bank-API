c1199cd71bde1bb5a19a6b9a729778c10b976fa9
Joseph Rey R. Panti <josephreyramirezpanti+github@gmail.com>
2026-05-28 20:50:10 +0800

    feat: prisma orm

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import {prisma} from "./src/db.ts";
 import dotenv from "dotenv";
 
 dotenv.config();
