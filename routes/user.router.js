ed7a38038afc118b50cb6e622435c846e945e919
Joseph Rey R. Panti <josephreyramirezpanti+github@gmail.com>
2026-05-31 16:05:51 +0800

    fix: updated database

--- routes/user.router.js
+++ routes/user.router.js
@@ -3,9 +3,10 @@ import {CreateUser} from "../controllers/user.controller.js";
 import Validate from "../utils/validate.util.js";
 import {UserSchemaValidator} from "../utils/validators/user.validate.js";
 import {ProtectRoute} from "../middleware/auth.middleware.js";
+import {Authorize} from "../middleware/authorize.middleware.js";
 
 const router = express.Router();
 
-router.post("/", ProtectRoute, Validate(UserSchemaValidator), CreateUser)
+router.post("/", ProtectRoute, Authorize, Validate(UserSchemaValidator), CreateUser)
 
 export default router;
