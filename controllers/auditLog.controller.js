51aa6d44e41618026f32c6b05ebd285e0b1e4501
Joseph Rey R. Panti <josephreyramirezpanti+github@gmail.com>
2026-06-05 11:42:00 +0800

    feat: Added query function on FetchAuditLogs

--- controllers/auditLog.controller.js
+++ controllers/auditLog.controller.js
@@ -2,10 +2,33 @@ import { prisma } from '../db/db.ts';
 
 export const FetchAuditLogs = async (req, res) => {
     try {
+        const {
+            modified_by,
+            action_performed,
+            table_name,
+            start_date,
+            end_date,
+            page=1,
+            limit=15,
+        } = req.query;
+
         const logs = await prisma.audit_log.findMany({
             orderBy: {
                 performed_at: 'desc'
             },
+            where: {
+                action_performed,
+                table_name,
+                performed_at: {
+                  lte: start_date ? new Date(start_date) : undefined,
+                  gte: end_date ? new Date(end_date) : undefined,
+                },
+                user: {
+                    name: modified_by
+                }
+            },
+            skip: (Number(page) - 1) * Number(limit),
+            take: Number(limit),
             include: {
                 user: {
                     select: {
