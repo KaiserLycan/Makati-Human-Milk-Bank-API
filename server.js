238f4d68c9e20bfbfbaf936c0dea19edfd50bb0e
Rolando Zagala Jr <rczagala@mymail.mapua.edu.ph>
2026-06-07 23:58:33 +0800

    feat: enhance CreateMilkPool controller with earliest expiration date and improved error handling

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ dotenv.config();
 
 import UserRouter from "./routes/user.router.js";
 import AuthRouter from "./routes/auth.router.js";
+import PoolingRouter from "./routes/pooling.router.js";
 import AuditLogRouter from "./routes/auditLog.router.js";
 import DonorRouter from "./routes/donor.router.js";
 import PrePoolRouter from "./routes/prepool.router.js";
@@ -25,8 +26,9 @@ app.use(express.urlencoded({ extended: false }));
 
 Swagger(app, port)
 
-app.use("/api/auth", AuthRouter);
+app.use("/api/auth", AuthRouter); 
 app.use("/api/users", UserRouter);
+app.use("/api/pooling", PoolingRouter);
 app.use("/api/audit-logs", AuditLogRouter);
 app.use("/api/donors", DonorRouter);
 app.use("/api/prepool", PrePoolRouter);
@@ -40,3 +42,5 @@ app.use("/api/reservations", ReservationRouter);
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
 })
+
+export default app;
