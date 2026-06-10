68d19e1d5ec5360921c03e9983d0ea5b3b42e15d
s1ncity <syjyang@mymail.mapua.edu.ph>
2026-06-10 15:57:13 +0800

    docs: added docs for get function of dashboard

--- routes/dashboard.router.js
+++ routes/dashboard.router.js
@@ -4,7 +4,37 @@ import { GetDashboardMetrics } from "../controllers/dashboard.controller.js";
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Dashboard
+ *   description: API for retrieving system metrics and generating reports
+ */
 
+/**
+ * @swagger
+ * /api/dashboard/summary:
+ *   get:
+ *     summary: Get weekly, monthly, or yearly system metrics (R62)
+ *     tags: [Dashboard]
+ *     security:
+ *       - cookieAuth: []
+ *     parameters:
+ *       - in: query
+ *         name: range
+ *         required: true
+ *         schema:
+ *           type: string
+ *           enum: [week, month, year]
+ *         description: The timeframe for the dashboard metrics
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved dashboard metrics.
+ *       400:
+ *         description: Invalid timeframe range provided.
+ *       500:
+ *         description: Internal Server Error.
+ */
 router.get("/summary", ProtectRoute, GetDashboardMetrics);
 
 export default router;
