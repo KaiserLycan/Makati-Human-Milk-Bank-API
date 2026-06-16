d5023bbcd495945bec3e764b0501cb25c6a5790a
Joseph Rey R. Panti <josephreyramirezpanti+github@gmail.com>
2026-06-16 09:38:19 +0800

    refactor: moved beneficiary.service.js to the correct folder fix: uploading an image wasn't working due to validation issues. fix: updating a singular image wasn't working. feat: when user updates an image, it gets deleted in cloudinary. fix: idSchemas.js doesn't validate bid only dtn

--- schemas/BeneficiarySchema.js
+++ schemas/BeneficiarySchema.js
@@ -2,18 +2,23 @@ import { z } from "zod";
 import { standardListQuery } from "./querySchemas.js";
 
 export const beneficiarySchema = z.object({
-    name: z.string().min(2, { error: "Name must be at least 2 characters" }),
-    caregiver: z.string().min(2, { error: "Caregiver's name must be at least 2 characters" }),
-    caregiver_email: z.email({ error: "Invalid email address" }),
-    caregiver_phone: z.e164({ error: "Invalid phone number" }),
-    birth_date: z.coerce.date(),
-    weight_kg: z.coerce.number(),
-    feeding_requirement_ml: z.coerce.number(),
-    profile: {
-        profile_image_url: z.url().optional(),
-        prescription_details: z.url().optional(),
-        clinical_abstract: z.url().optional(),
-    },
+    name: z.string().min(2, { error: "Name must be at least 2 characters" }).optional(),
+    caregiver: z
+        .string()
+        .min(2, { error: "Caregiver's name must be at least 2 characters" })
+        .optional(),
+    caregiver_email: z.email({ error: "Invalid email address" }).optional(),
+    caregiver_phone: z.e164({ error: "Invalid phone number" }).optional(),
+    birth_date: z.coerce.date().optional(),
+    weight_kg: z.coerce.number().optional(),
+    feeding_requirement_ml: z.coerce.number().optional(),
+    profile: z
+        .object({
+            profile_image_url: z.union([z.literal(""), z.string().url()]).optional(),
+            prescription_details: z.union([z.literal(""), z.string().url()]).optional(),
+            clinical_abstract: z.union([z.literal(""), z.string().url()]).optional(),
+        })
+        .optional(),
 });
 
 export const beneficiaryQuerySchema = standardListQuery.extend({
